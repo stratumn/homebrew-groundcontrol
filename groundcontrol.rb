2440aa52fc045e54757a1757d9af47205bb81172
goreleaserbot <goreleaser@carlosbecker.com>
2019-02-07 13:45:08 +0100

    Brew formula update for groundcontrol version v0.2.3

--- groundcontrol.rb
+++ groundcontrol.rb
@@ -1,9 +1,9 @@
 class Groundcontrol < Formula
   desc ""
   homepage ""
-  url "https://github.com/stratumn/groundcontrol/releases/download/v0.2.2/groundcontrol_0.2.2_Darwin_x86_64.tar.gz"
-  version "0.2.2"
-  sha256 "9e557206e424b5ff114ee99f92305753c46871a8a79c925bb923b06e1138d07d"
+  url "https://github.com/stratumn/groundcontrol/releases/download/v0.2.3/groundcontrol_0.2.3_Darwin_x86_64.tar.gz"
+  version "0.2.3"
+  sha256 "971cc762ad22f0b9f862a060b007a19bff4f70f147a8f50593a832cfddb5d996"
 
   def install
     bin.install "groundcontrol"
